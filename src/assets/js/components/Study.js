ddbe587afd4873cca6147d32c2db75a0a6af9222
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-30 15:09:54 +0200

    Mark up nstudy section

---
+++ src/assets/js/components/Study.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Timeline from './Timeline.js'
+
+function Study() {
+  return (
+    <section id='study'>
+      <div className='container'>
+        <h3 className='section-title'>Percorso formativo</h3>
+        <Timeline />
+      </div>
+    </section>
+  )
+}
+
+export default Study
