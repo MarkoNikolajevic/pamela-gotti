ac5a054350df9232201cbd86a5e3c57bb3f5f154
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-30 15:10:13 +0200

    Mark up ntimeline

---
+++ src/assets/js/components/Timeline.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+      <div className='timeline-container left'>
+        <div className='content'>
+          <p className='date'>2009</p>
+        </div>
+      </div>
+      <div className='timeline-container right'>
+        <div className='content'>
+          <p className='studies'>
+            Diploma di maturità magistrale conseguito presso l’Ist.Magistrale
+            “G. Carducci” di Trieste, con la valutazione di 60/60
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Timeline
