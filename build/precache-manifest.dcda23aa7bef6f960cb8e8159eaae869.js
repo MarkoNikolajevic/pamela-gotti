29a199b44bd0e9b72dba5f1b009ef86412ae9449
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-05-30 22:56:22 +0200

    Add build folder

---
+++ build/precache-manifest.dcda23aa7bef6f960cb8e8159eaae869.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5e930bd7600e579e11d29f5410879e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc66dfb7d7cd7d135d1",
+    "url": "/static/css/main.6d5bb1e2.chunk.css"
+  },
+  {
+    "revision": "7ca78824146b52a16a79",
+    "url": "/static/js/2.77504f2a.chunk.js"
+  },
+  {
+    "revision": "6cc66dfb7d7cd7d135d1",
+    "url": "/static/js/main.1d868a9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "72e9461f5b9e9eaf9961adb793ff86ba",
+    "url": "/static/media/disturbi.72e9461f.svg"
+  },
+  {
+    "revision": "3c1ae2fc4389cb961bd7d64408a639f2",
+    "url": "/static/media/gravidanza.3c1ae2fc.svg"
+  },
+  {
+    "revision": "b9f0b0dc16b93f67850e3f63871dc5fd",
+    "url": "/static/media/ipnosi.b9f0b0dc.svg"
+  },
+  {
+    "revision": "6bb0a3cf38a48bd0e82865e31480169f",
+    "url": "/static/media/marko-nikolajevic-logo.6bb0a3cf.svg"
+  },
+  {
+    "revision": "5db2517eddb5c3257b97367585836f8d",
+    "url": "/static/media/profile-pic.5db2517e.jpg"
+  },
+  {
+    "revision": "a54d9e47adf80bdd4e35a17f9bf52120",
+    "url": "/static/media/psicosomatica.a54d9e47.svg"
+  }
+]);
