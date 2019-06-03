daf2b7310a548083c52fdc53b909769e87f3624b
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-06-03 13:48:16 +0200

    Add pubblications

--- src/assets/js/components/Study.js
+++ src/assets/js/components/Study.js
@@ -7,6 +7,16 @@ function Study() {
       <div className='container'>
         <h3 className='section-title'>Percorso formativo</h3>
         <Timeline />
+        <div className='pubblications'>
+          <h3 className='section-title'>Pubblicazioni</h3>
+          <ul className='pubblications-list'>
+            <li>Clarici A., Gotti P., Travan L. & Bava A. (2002) A single case study of anterograde amnesia due to a right-hemisphere lesion presented from a depth neuropsycology perspective. Forum 2002 of European of Neuroscience Parigi 12-18 luglio 2002 (abstract)</li>
+            <li>L'attesa (LaPiccolaVolante, 2013, Cagliari) – opera di narrativa</li>
+            <li>Fuori in AA.VV. Monday Blues (Edizioni GutenbergLab- 2013) – racconto breve</li>
+            <li>Fuori di 3 (LaPiccolaVolante, 2015, Cagliari)- opera narrativa</li>
+            <li>Tra le mura in AA.VV Il cerchio dele streghe (Morganti editore 2017, Udine)- racconto narrativo</li>
+          </ul>
+        </div>
       </div>
     </section>
   )
