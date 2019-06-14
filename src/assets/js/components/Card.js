078ab58c6aa9d4fa643f08ef9efc39a01205a2a3
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-06-14 22:59:43 +0200

    Add wrapper

--- src/assets/js/components/Card.js
+++ src/assets/js/components/Card.js
@@ -4,13 +4,15 @@ function Card(props) {
   return (
     <div className='card'>
       <img className='card-img' src={props.card.image} alt={props.card.imgAlt}/>
-      <h4 className='card-title'>{props.card.title}</h4>
-      <p className='card-info'>
-        {props.card.info}
-        <a href={props.card.keepReading} className='card-link' target='_blank' rel='noopener noreferrer'>
-          {props.card.linkText}
-        </a>
-      </p>
+      <div className='card-text'>
+        <h4 className='card-title'>{props.card.title}</h4>
+        <p className='card-info'>
+          {props.card.info}
+          <a href={props.card.keepReading} className='card-link' target='_blank' rel='noopener noreferrer'>
+            {props.card.linkText}
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
--- src/assets/js/components/Therapy.js
+++ src/assets/js/components/Therapy.js
@@ -11,44 +11,53 @@ function Therapy() {
       <div className='container'>
         <h3 className='section-title'>Di cosa mi occupo</h3>
         <div className='divider'></div>
-        <Card
-          card={{
-            image: psicosomatica,
-            imgAlt: 'Psicosomatica e complessità',
-            title: 'Psicosomatica e complessità',
-            info: "Ognuno ha la sua storia. Qualcuno ci ha detto che l'abbiamo ereditata dai nostri genitori, nonni e avi. Altri ci dicono che dipende dalla nostra volontà, dal nostro pensiero. Altri ancora sostengono che dipenda dal caso e naturalmente qualcun altro ritiene al contrario che sia tutto scritto nel nostro destino o dal nostro karma.",
-            keepReading: '#',
-            linkText: 'Continua a leggere'
-          }}
-        />
-        <Card
-          card={{
-            image: ipnosi,
-            imgAlt: 'Ipnosi',
-            title: 'Ipnosi',
-            info: "Le opinioni che molte persone hanno sull’ipnosi si basano su convinzioni e luoghi comuni appresi dai romanzi, dai film, dagli spettacoli televisivi e dalle proprie stesse fantasie. Alcuni ne sono addirittura spaventati, pensando che si tratti di un’esperienza di perdita di controllo, in cui ci si trova succubi e alla mercé dell’ipnotizzatore.",
-            keepReading: '#',
-            linkText: 'Continua a leggere'
-          }}
-        />
-        <Card
-          card={{
-            image: gravidanza,
-            imgAlt: 'Gravidanza',
-            title: 'Preparazione al parto',
-            info: "Training di preparazione al parto mediante induzione ipnotica. Una donna che si prepara a partorire porta con sé credenze e convinzioni di vario tipo, caricate emotivamente da ansie anticipatorie e da una certa confusione rispetto a ciò che dovrà effettivamente accadere.",
-            keepReading: '#',
-            linkText: 'Continua a leggere'
-          }}
-        />
-        <Card
-          card={{
-            image: disturbi,
-            imgAlt: 'Disturbi trattati',
-            title: 'Disturbi trattati',
-            info: "- Depressione - Ansia - Attacchi di panico - Disturbi alimentari - Disturbi sessuali - Dipendenze",
-          }}
-        />
+        <div className='card-wrapper'>
+          <Card
+            card={{
+              image: psicosomatica,
+              imgAlt: 'Psicosomatica e complessità',
+              title: 'Psicosomatica e complessità',
+              info: "Ognuno ha la sua storia. Qualcuno ci ha detto che l'abbiamo ereditata dai nostri genitori, nonni e avi. Altri ci dicono che dipende dalla nostra volontà, dal nostro pensiero. Altri ancora sostengono che dipenda dal caso e naturalmente qualcun altro ritiene al contrario che sia tutto scritto nel nostro destino o dal nostro karma.",
+              keepReading: '#',
+              linkText: 'Continua a leggere'
+            }}
+          />
+          <Card
+            card={{
+              image: ipnosi,
+              imgAlt: 'Ipnosi',
+              title: 'Ipnosi',
+              info: "Le opinioni che molte persone hanno sull’ipnosi si basano su convinzioni e luoghi comuni appresi dai romanzi, dai film, dagli spettacoli televisivi e dalle proprie stesse fantasie. Alcuni ne sono addirittura spaventati, pensando che si tratti di un’esperienza di perdita di controllo, in cui ci si trova succubi e alla mercé dell’ipnotizzatore.",
+              keepReading: '#',
+              linkText: 'Continua a leggere'
+            }}
+          />
+          <Card
+            card={{
+              image: gravidanza,
+              imgAlt: 'Gravidanza',
+              title: 'Preparazione al parto',
+              info: "Training di preparazione al parto mediante induzione ipnotica. Una donna che si prepara a partorire porta con sé credenze e convinzioni di vario tipo, caricate emotivamente da ansie anticipatorie e da una certa confusione rispetto a ciò che dovrà effettivamente accadere.",
+              keepReading: '#',
+              linkText: 'Continua a leggere'
+            }}
+          />
+          <Card
+            card={{
+              image: disturbi,
+              imgAlt: 'Disturbi trattati',
+              title: 'Disturbi trattati',
+              info: `
+                    Depressione
+                    Ansia
+                    Attacchi di panico
+                    Disturbi alimentari
+                    Disturbi sessuali
+                    Dipendenze
+                    `
+            }}
+          />
+        </div>
       </div>
     </section>
   )
