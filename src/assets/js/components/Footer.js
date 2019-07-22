c510ef93fea8d124ae2b4cd0bfe7b774f726bc34
Marko Nikolajevic <mrknikolajevic@gmail.com>
2019-07-22 22:58:26 +0200

    Add privacy policy

--- src/assets/js/components/Footer.js
+++ src/assets/js/components/Footer.js
@@ -1,25 +1,63 @@
 import React from 'react'
 import markoNikolajevicLogo from '../../img/marko-nikolajevic-logo.svg'
+import ReactModal from 'react-modal'
+import Privacy from './Privacy'
 
-function Footer() {
-  return (
-    <footer className='footer'>
-      <div className='footer-content'>
-        <p>&copy; 2019 Dott.ssa Pamela Gotti</p>
-        <p>Via di Donota, 28 34121 Trieste</p>
-        <p>345-1660590</p>
-        <p>pamelagotti@libero.it</p>
-        <p>www.pamelagotti.it</p>
-        <p>P.IVA 01275270328</p>
-        <br />
-        <p>creato da
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+    const fab = document.querySelector('.fab-btn');
+    fab.style.visibility = 'hidden';
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+    const fab = document.querySelector('.fab-btn');
+    fab.style.visibility = 'visible';
+  }
+
+  render() {
+    return (
+      <footer className='footer'>
+        <div className='footer-content'>
+          <p>&copy; 2019 Dott.ssa Pamela Gotti</p>
+          <p>Via di Donota, 28 34121 Trieste</p>
+          <p>345-1660590</p>
+          <p>pamelagotti@libero.it</p>
+          <p>www.pamelagotti.it</p>
+          <p>P.IVA 01275270328</p>
+          <ReactModal
+            closeTimeoutMS={500}
+            onRequestClose={() => this.toggleModal()}
+            isOpen={this.state.showModal}
+            contentLabel='Informativa Privacy'
+            className='modal'
+            overlayClassName='overlay'
+            ariaHideApp={false}
+          >
+            <button className='modal-btn close-modal-fixed-btn' onClick={this.handleCloseModal}><i className="material-icons">close</i></button>
+            <Privacy />
+          </ReactModal>
+          <p className='privacy' onClick={this.handleOpenModal}>Informativa Privacy</p>
+          <br />
+          <p>creato da
           <a href='https://www.markonikolajevic.com/' target='_blank' rel='noopener noreferrer'>
-            <img className='dev-logo' src={markoNikolajevicLogo} alt='Marko Nikolajevic logo'/>
-          </a>
-        </p>
-      </div>
-    </footer>
-  )
+              <img className='dev-logo' src={markoNikolajevicLogo} alt='Marko Nikolajevic logo' />
+            </a>
+          </p>
+        </div>
+      </footer>
+    )
+  }
 }
 
 export default Footer
---
+++ src/assets/js/components/Privacy.js
@@ -0,0 +1,191 @@
+import React from 'react'
+
+function Privacy() {
+  return (
+    <div>
+      <h1>Informativa sul trattamento dei dati personali e sui cookie del sito internet</h1>
+      <h2>1. Premessa</h2>
+      <p>
+        In ottemperanza degli obblighi derivanti dal Regolamento (UE) n. 2016/679 del Parlamento Europeo e del Consiglio del 27 aprile
+        2016 (di seguito, il 'Regolamento' o 'GDPR') e della legislazione nazionale vigente, Dott.ssa Pamela Gotti rispetta e tutela i dati
+        personali dei visitatori e degli utenti (di seguito, gli 'interessati') del sito internet www.pamelagotti.it (di seguito, il 'Sito Internet').
+      </p>
+      <p>
+        Questo documento fornisce informazioni sul trattamento dei dati personali raccolti da Dott.ssa Pamela Gotti tramite il Sito Internet e , pertanto,
+        costituisce un'informativa agli Interessati ai sensi della predetta normativa e non si applica ai dati raccolti dalla Dott.ssa Pamela Gotti tramite
+        canali diversi dal Sito Internet.
+      </p>
+      <p>
+        Secondo quanto previsto dal Regolamento, il trattamento dei dati personali degli Interessati viene effettuato nel rispetto dei principi di correttezza, 
+        liceità, trasparenza e tutela della riservatezza.
+      </p>
+      <p>
+        Il Sito Internet contiene collegamenti ad altri siti internet: la presente informativa non riguarda tali altri siti internet, eventualmente consultati 
+        dagli Interessati tramite appositi link. Tali siti internet possono contenere informative sul trattamento dei dati personali difformi, in tutto o in parte, 
+        da questa informativa. Dott.ssa Pamela Gotti invita pertanto gli Interessati a leggere con attenzione le informative privacy di ciascun altro sito al quale 
+        si collegano, specialmente prima di inserire una qualsiasi informazione personale.
+      </p>
+      <h2>2. Identità e dati di contatto del titolare del trattamento</h2>
+      <p>
+        Il titolare del trattamento è Dott.ssa Pamela Gotti (di seguito, <strong>“Dott.ssa Pamela Gotti”</strong> o il <strong>“Titolare del Trattamento”</strong>),
+        C.F. e P. IVA GTTPML78T51L424L / 01275270328, con sede legale in Via Muzio Girolamo, 11, 34143 Trieste, Trieste, cell: 345 1660590;
+        email: pamelagotti@libero.it
+      </p>
+      <h2>3. Tipologie di dati trattati tramite il sito internet</h2>
+      <p>Il Titolare del trattamento, tramite il Sito Internet, potrà trattare i seguenti dati:</p>
+      <h3>Dati raccolti in maniera automatizzata - Dati di traffico e navigazione</h3>
+      <p>
+        I sistemi informatici e le procedure software preposte al funzionamento del Sito Internet acquisiscono, nel corso del loro normale esercizio, alcuni dati 
+        personali la cui trasmissione è implicita nell'uso dei protocolli di comunicazione internet.
+      </p>
+      <p>
+        In questa categoria di dati rientrano gli indirizzi IP o i nomi a dominio dei computer e dei terminali utilizzati dagli utenti, il tipo di browser, il nome
+        dell’Internet Service Provider, gli indirizzi in notazione URI/URL (Uniform Resource Identifier/Locator) delle risorse richieste, la data e l’orario di visita
+        del Sito Internet, il metodo utilizzato nel sottoporre la richiesta al server, la dimensione del file ottenuto in risposta, il codice numerico indicante lo stato
+        della risposta data dal server (buon fine, errore, ecc.), la pagina web di provenienza dell’utente e quella di uscita ed altri parametri relativi al sistema operativo
+        e all'ambiente informatico dell'utente.
+      </p>
+      <h3>Dati comunicati dagli interessati</h3>
+      <p>
+        L'invio facoltativo, esplicito e volontario di messaggi agli indirizzi di contatto del Titolare del Trattamento, nonché la compilazione e l'inoltro dei moduli di contatto
+        presenti sul Sito Internet, comportano l'acquisizione dei dati di contatto del mittente, necessari a rispondere, nonché di tutti i dati personali inclusi nelle comunicazioni
+        (quali, a titolo meramente esemplificativo, nome, cognome, indirizzo email…).
+      </p>
+      <p>
+        In ogni caso, gli Interessati sono tenuti a fornire dati veritieri e precisi e ad informare tempestivamente il Titolare del Trattamento di eventuali successive modifiche.
+      </p>
+      <h2>4. Cookie e altri sistemi di tracciamento</h2>
+      <p>
+        Il Sito Internet fa uso di cookie e tecnologie similari. I cookie permettono di conservare informazioni sulle preferenze dei visitatori, consentono di verificare il corretto
+        funzionamento del sito e di migliorarne le funzionalità personalizzando il contenuto delle pagine in base al tipo del browser utilizzato, oppure per semplificarne la navigazione
+        automatizzando le procedure (es. Login, lingua sito) e per l’analisi dell’uso del sito da parte dei visitatori.
+      </p>
+      <p>Il Sito Internet fa uso delle seguenti categorie di cookie:</p>
+      <h3>Cookie tecnici</h3>
+      <p>
+        I cookie di questa categoria comprendono sia cookie persistenti che cookie di sessione, consentono di distinguere tra gli utenti collegati evitando che un servizio sia erogato
+        all’utente sbagliato e quindi sono conseguenza di una espressa richiesta dell’utente, e inoltre sono utilizzati per la sicurezza del sito e degli utenti stessi. In assenza di
+        tali cookie, il sito o alcune porzioni di esso potrebbero non funzionare correttamente.
+      </p>
+      <p>
+        I cookie di questa categoria vengono sempre inviati dal nostro dominio, e per essi non occorre consenso degli Interessati.
+      </p>
+      <h3>Cookie analitici</h3>
+      <p>
+        I cookie di questa categoria sono utilizzati per raccogliere informazioni sul corretto utilizzo del sito e sul comportamento degli utenti a fini di analisi statistica,
+        per migliorare il sito e semplificarne l’utilizzo. Questo tipo di cookie raccoglie informazioni in forma anonima sull’attività degli utenti nel sito e sul modo in cui
+        sono arrivati al sito e alle pagine visitate. I cookie di questa categoria vengono inviati dal sito stesso o da domini di terze parti.
+      </p>
+      <h3>Cookie di terze parti</h3>
+      <p>
+        Il Sito Internet funge anche da intermediario per cookie di terze parti, utilizzati per poter fornire ulteriori servizi e funzionalità ai visitatori e per migliorare l’uso
+        del sito stesso, come i pulsanti per i social. Alcuni di questi cookie sono di profilazione, cioè utilizzati dalle terze parti per raccogliere informazioni sul comportamento
+        e gli interessi degli utenti al fine di fornire pubblicità personalizzata.
+      </p>
+      <p>
+        <strong>Utilizzando il Sito Internet, continuando la navigazione del Sito Internet, oppure cliccando sul banner presente al primo accesso al sito, l’utente acconsente
+        espressamente all’uso dei cookie e delle tecnologie similari,</strong> e in particolare alla registrazione di tali cookie sul suo terminale per le finalità sopra indicate,
+        oppure all’accesso tramite i cookie a informazioni sul suo terminale.
+      </p>
+      <p>
+        L’utente può rifiutare l’utilizzo dei cookie e in qualsiasi momento può revocare un consenso già fornito. Poiché i cookie sono collegati al browser utilizzato, 
+        <strong>possono essere disabilitati direttamente dal browser</strong>, così rifiutando/revocando il consenso all’uso dei cookie.
+      </p>
+      <p>
+        <strong>La disabilitazione dei cookie potrebbe impedire il corretto utilizzo di alcune funzioni del sito stesso</strong>, in particolare i servizi forniti da terze parti potrebbero
+        non essere accessibili, e quindi potrebbero non essere visualizzabili, a titolo esemplificativo, video di YouTube o altri servizi di condivisione video; i social button dei social network; le mappe di Google.
+      </p>
+      <p>
+        Nel caso in cui l’utente dovesse negare e/o revocare il consenso all’utilizzo dei cookie, l’esperienza di navigazione sul Sito Internet potrebbe essere compromessa.
+      </p>
+      <p>
+        La presente privacy policy non si applica ai servizi forniti da terze parti, e questo sito non ha alcun controllo sui loro cookie, interamente gestiti dalle terze parti, e non ha alcun accesso alle informazioni
+        raccolte tramite detti cookie. In conseguenza di ciò le informazioni sull’uso dei cookie di terze parti, sulle finalità degli stessi, nonché sulle modalità per l’eventuale disabilitazione, sono fornite direttamente
+        dalle terze parti alle pagine gestite da tali soggetti terzi.
+        In particolare, il Sito Internet utilizza cookie delle seguenti terze parti: Google Analytics, Google Maps, Youtube, Facebook; PayPal Inc.
+      </p>
+      <h2>5. Finalità e base giuridica del trattamento</h2>
+      <p>Il Titolare del Trattamento tratta i dati di traffico e di navigazione per le seguenti finalità:</p>
+      <ul>
+        <li>Gestire, amministrare e migliorare il Sito Internet; controllare il corretto funzionamento dei servizi offerti;</li>
+        <li>Adempiere agli obblighi previsti dalla legge e/o da regolamenti e/o da ordini dell’Autorità Giudiziaria;</li>
+        <li>Prevenire e/o rilevare attività fraudolente e/o dannose per il Sito Internet;</li>
+        <li>Effettuare analisi con finalità tecniche e/o commerciali; ottenere informazioni statistiche sull'uso dei servizi (pagine più visitate, numero di visitatori per fascia oraria o giornaliera, aree geografiche di provenienza, ecc..). Il trattamento di tali dati è necessario per poter navigare sul Sito Internet.</li>
+      </ul>
+      <p>Il Titolare del Trattamento tratta i dati comunicati dagli Interessati per le seguenti finalità:</p>
+      <ul>
+        <li>Rispondere alle richieste di assistenza e, in generale, a qualsiasi domanda e/o richiesta avanzata dagli utenti;</li>
+        <li>Inviare agli utenti email di tipo amministrativo e/o di supporto tecnico (a titolo meramente esemplificativo, note tecniche, promemoria, aggiornamenti..);</li>
+        <li>Inviare a mezzo posta e/o via email newsletter, comunicazioni commerciali e/o materiale pubblicitario su prodotti e/o servizi offerti dal Titolare del Trattamento.</li>
+      </ul>
+      <p>
+        Il trattamento dei dati comunicati dagli Interessati per le finalità sopra indicate richiede il consenso degli Interessati. Tale consenso è sempre facoltativo ma, in difetto, Dott.ssa Pamela Gotti non potrà trattare i dati raccolti per le predette finalità.
+      </p>
+      <h2>6. Comunicazione dei dati</h2>
+      <p>
+        I dati personali raccolti potranno essere comunicati a organismi di vigilanza, autorità giudiziarie nonché a tutti i soggetti ai quali la comunicazione sia obbligatoria
+        per legge e/o necessario per l’espletamento delle finalità sopra descritte.
+      </p>
+      <h2>7. Modalità di trattamento dei dati personali raccolti e periodo di conservazione</h2>
+      <p>I dati raccolti posso essere sottoposti a trattamento sia cartaceo che elettronico e/o automatizzato.</p>
+      <p>In ogni caso, il Titolare del Trattamento tratterà i dati personali raccolti per il tempo necessario per adempiere alle finalità di cui alla presente informativa e, 
+        in ogni caso, per un periodo non eccedente a quanto richiesto dalla normativa vigente (ivi inclusa la normativa in materia fiscale).
+      </p>
+      <h2>8. Eventuale trasferimento dei dati personali</h2>
+      <p>
+        La gestione e la conservazione dei dati avverrà su server ubicati all’interno dell’Unione Europea del Titolare del Trattamento e/o di società terze incaricate e debitamente nominate quali Responsabili del trattamento.
+        Attualmente i server sono situati in Italia.
+      </p>
+      <p>
+        Resta in ogni caso inteso che il Titolare del Trattamento, ove si rendesse necessario, avrà facoltà di spostare l’ubicazione dei server in Italia e/o Unione Europea e/o Paesi extra-UE. In tal caso, il Titolare del Trattamento assicura
+        sin d’ora che il trasferimento dei dati extra-UE avverrà in conformità alle disposizioni di legge applicabili stipulando, se necessario, accordi che garantiscano un livello di protezione adeguato e/o adottando le clausole contrattuali
+        standard previste dalla Commissione Europea.
+      </p>
+      <h2>9. Misure di sicurezza</h2>
+      <p>Il Titolare del Trattamento tratta i dati degli Interessati in maniera lecita e corretta, adottando le opportune misure di sicurezza volte ad impedire accessi non autorizzati, divulgazione, modifica o distruzione non autorizzata dei dati, nonché usi illeciti dei dati.</p>
+      <p>Il trattamento viene effettuato mediante strumenti informatici e/o telematici, con modalità organizzative e con logiche strettamente correlate alle finalità indicate, e i dati sono memorizzati e conservati in strutture sicure con limitazioni di accesso e verifica del personale.</p>
+      <p>L’accesso alla informazioni è rigorosamente limitato al personale autorizzato.</p>
+      <p>Il Sito Internet è costantemente monitorato per verificare eventuali violazioni della sicurezza.</p>
+      <p>Oltre al Titolare del Trattamento, in alcuni casi, potrebbero avere accesso ai dati categorie di incaricati coinvolti nell’organizzazione del Sito Internet (personale amministrativo, commerciale, marketing, legali, amministratori di sistema) ovvero soggetti esterni (come fornitori di servizi
+        tecnici terzi, corrieri postali, hosting provider, società informatiche, agenzie di comunicazione), che agiranno sulla base delle specifiche istruzioni fornite dal Titolare del Trattamento.
+      </p>
+      <p>
+        In ogni caso, il Titolare del Trattamento invita gli Interessati ad adottare idonee protezioni e/o cautele contro l’accesso non autorizzato alla propria area riservata e/o al proprio computer.
+      </p>
+      <h2>10. Diritti degli interessati</h2>
+      <p>
+        In conformità a quanto previsto dal capo III del GDPR, gli Interessati possono essere esercitare in qualsiasi momento i diritti ivi previsti e in particolare:
+      </p>
+      <h3>Diritto di accesso</h3>
+      <p>
+        Ottenere dal Titolare conferma che sia o meno in corso un trattamento di Dati Personali che li riguardano e, in tal caso, ricevere informazioni circa le finalità del trattamento, le categorie dei Dati coinvolti, i destinatari o le categorie dei destinatari cui i Dati Personali sono o saranno comunicati,
+        il periodo di conservazione dei Dati Personali oppure i criteri per determinare tale periodo ( art. 15, GDPR);
+      </p>
+      <h3>Diritto di rettifica</h3>
+      <p>Ottenere dal Titolare, senza ingiustificato ritardo, la rettifica dei Dati Personali inesatti e l’integrazione dei Dati Personali incompleti fornendo anche una dichiarazione integrativa (art. 16, GDPR);</p>
+      <h3>Diritto di cancellazione</h3>
+      <p>
+        Ottenere dal Titolare, senza ingiustificato ritardo, la cancellazione dei Dati Personali, nei casi previsti dal GDPR (c.d. “diritto all’oblio” – art. 17, GDPR);
+      </p>
+      <h3>Diritto di limitazione</h3>
+      <p>Ottenere dal Titolare la limitazione del trattamento, nei casi previsti dal GDPR (art. 18, GDPR);</p>
+      <h3>Diritto alla portabilità</h3>
+      <p>Ricevere dal Titolare in un formato strutturato, di uso comune e leggibile da dispositivo automatico, i Dati Personali che li riguardano forniti al titolare del trattamento e richiedere di trasmetterli direttamente, o per mezzo del titolare se tecnicamente fattibile, ad un altro titolare del trattamento (c.d. “diritto alla portabilità dei dati” – art. 20 GDPR);</p>
+      <h3>Diritto di opposizione</h3>
+      <p>Opporsi, al ricorrere di situazioni particolari che li riguardano, al trattamento dei Dati Personali nonché al trattamento di Dati Personali per finalità di marketing diretto (art. 21 GDPR).L'apposita istanza può essere presentata contattando in qualsiasi momento Dott.ssa Pamela Gotti a mezzo posta all’indirizzo: Dott.ssa Pamela Gotti Via Muzio Girolamo, 11, 34143 Trieste, Trieste; a mezzo posta elettronica all’indirizzo pamelagotti@libero.it.</p>
+      <p>Con le stesse modalità possono essere revocati in qualsiasi momento i consensi espressi in riferimento alla presente informativa senza pregiudicare la liceità del trattamento basato sul consenso prestato prima della revoca.</p>
+      <p>Le eventuali comunicazioni e le azioni intraprese da Dott.ssa Pamela Gotti a fronte dell’esercizio dei diritti di seguito elencati, saranno effettuate gratuitamente, fatta eccezione per i casi previsti dall’art. 12, comma 5, del GDPR.</p>
+      <p>Gli interessati possono altresì contattare Dott.ssa Pamela Gotti al numero di telefono 345 1660590 per il caso in cui necessitassero di informazioni e/o chiarimenti in merito al trattamento dei dati personali effettuati tramite il Sito Internet.</p>
+      <h2>11. Diritto di reclamo</h2>
+      <p>Gli Interessati che ritengono che il trattamento dei dati personali a loro riferiti effettuato attraverso il Sito Internet avvenga in violazione di quanto previsto dal Regolamento hanno il diritto di proporre reclamo al Garante, come previsto dall'art. 77 del Regolamento stesso, o di adire le opportune sedi giudiziarie (art. 79 del Regolamento).</p>
+      <h2>12. Tutela dei minori</h2>
+      <p>Il Titolare del Trattamento non permette a persone minori di età inferiore a 16 anni di usufruire dei suoi servizi e, pertanto, non raccoglie intenzionalmente informazioni relative ad essi. Qualora dovesse accorgersi di aver raccolto dati relativi a persone minori di anni 16, in assenza di un consenso genitoriale dimostrabile, provvederà a cancellare tali dati nel minor tempo possibile.</p>
+      <h2>13. Aggiornamenti periodici della presente informativa sulla privacy</h2>
+      <p>La presente informativa sulla privacy è valida ed efficace con decorrenza dal 25 maggio 2018 e potrà essere soggetta a modifiche nel tempo, anche per effetto di modifiche o integrazioni della vigente normativa.</p>
+      <p>Qualora il Titolare del Trattamento dovesse apportare qualsivoglia modifica significativa a tale documento, lo stesso ne darà informazione agli Interessati attraverso i mezzi che riterrà più idonei allo scopo (quale, a titolo meramente esemplificativo e non esaustivo, la pubblicazione sulla home page del Sito Internet e/o l’invio di una newsletter all’indirizzo email fornito dagli Interessati).</p>
+      <p>La presente informativa è stata aggiornata in data 25 maggio 2018.</p>
+    </div>
+  )
+}
+
+export default Privacy
