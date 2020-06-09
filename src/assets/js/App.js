dc64010a6a8a1831bfe48ce5cbf0ad007f1db24e
Marko Nikolajevic <mrknikolajevic@gmail.com>
2020-06-09 19:02:23 +0200

    add new address

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import '../style/App.sass'
-import Navbar from './components/Navbar.js'
-import NavMobile from './components/NavMobile.js'
-import AboutMe from './components/AboutMe.js'
-import Study from './components/Study.js'
-import Therapy from './components/Therapy.js'
-import Form from './components/Form.js'
-import Footer from './components/Footer.js'
+import React from 'react';
+import '../style/App.sass';
+import Navbar from './components/Navbar.js';
+import NavMobile from './components/NavMobile.js';
+import AboutMe from './components/AboutMe.js';
+import Study from './components/Study.js';
+import Therapy from './components/Therapy.js';
+import Form from './components/Form.js';
+import Footer from './components/Footer.js';
 import ScrollToTopBtn from './components/ScrollToTopBtn';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <NavMobile />
       <AboutMe />
@@ -20,8 +20,8 @@ function App() {
       <Form />
       <Footer />
       <ScrollToTopBtn />
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/assets/js/components/Footer.js
+++ src/assets/js/components/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import markoNikolajevicLogo from '../../img/marko-nikolajevic-logo.svg'
-import ReactModal from 'react-modal'
-import Privacy from './Privacy'
+import React from 'react';
+import markoNikolajevicLogo from '../../img/marko-nikolajevic-logo.svg';
+import ReactModal from 'react-modal';
+import Privacy from './Privacy';
 
 class Footer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false
+      showModal: false,
     };
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
@@ -29,8 +29,9 @@ class Footer extends React.Component {
     return (
       <footer className='footer'>
         <div className='footer-content'>
-          <p>&copy; 2019 Dott.ssa Pamela Gotti</p>
+          <p>&copy; 2019 - 2020 Dott.ssa Pamela Gotti</p>
           <p>Via di Donota, 28 34121 Trieste</p>
+          <p>Martedì - Piazza Giovanni XXIII, 15 Udine, fr. Cussignacco</p>
           <p>345-1660590</p>
           <p>pamelagotti@libero.it</p>
           <p>www.pamelagotti.com</p>
@@ -44,20 +45,36 @@ class Footer extends React.Component {
             overlayClassName='overlay'
             ariaHideApp={false}
           >
-            <button className='modal-btn close-modal-fixed-btn' onClick={this.handleCloseModal}><i className="material-icons">close</i></button>
+            <button
+              className='modal-btn close-modal-fixed-btn'
+              onClick={this.handleCloseModal}
+            >
+              <i className='material-icons'>close</i>
+            </button>
             <Privacy />
           </ReactModal>
-          <p className='privacy' onClick={this.handleOpenModal}>Informativa Privacy</p>
+          <p className='privacy' onClick={this.handleOpenModal}>
+            Informativa Privacy
+          </p>
           <br />
-          <p>creato da
-          <a href='https://www.markonikolajevic.com/' target='_blank' rel='noopener noreferrer'>
-              <img className='dev-logo' src={markoNikolajevicLogo} alt='Marko Nikolajevic logo' />
+          <p>
+            creato da
+            <a
+              href='https://www.markonikolajevic.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <img
+                className='dev-logo'
+                src={markoNikolajevicLogo}
+                alt='Marko Nikolajevic logo'
+              />
             </a>
           </p>
         </div>
       </footer>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
